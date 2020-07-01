bab13d06103acb82593f712bb47bb7fae66bfbd4
JIECHIEU KAMENI FLORENTIN FLAMBEAU <jkflorex@gmail.com>
2020-07-01 12:25:33 +0100

    Update process_data.py
    Update comments

--- process_data.py
+++ process_data.py
@@ -1,6 +1,6 @@
 __author__ = 'Ibaaslabs'
 from preprocessing.PreProcessor import Preprocessor
 p = Preprocessor("data/resume_samples.txt","data/classes_saved.txt","data/skills.txt")
-#p.transform_data3() # for resume to matrices
-#p.transform_data4() # For doc2vec enconding
-p.wc()
+#p.transform_data3() # transforms resumes to matrices
+#p.transform_data4() # transform resume to vectors using doc2vec enconding
+p.wc() # computes the min, max and the average word counts of resumes. 
